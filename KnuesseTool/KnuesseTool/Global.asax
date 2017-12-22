058106b7d432696f156b8371dcd17b16219dd4d7
Dario Menzel <nerdydi@gmail.com>
2017-12-22 14:22:18 +0100

    Initializing

---
+++ KnuesseTool/KnuesseTool/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KnuesseTool.MvcApplication" Language="C#" %>
---
+++ KnuesseTool/KnuesseTool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KnuesseTool.Startup))]
+namespace KnuesseTool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
